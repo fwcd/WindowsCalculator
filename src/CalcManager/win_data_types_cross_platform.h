b9c3e85813de020424a0841fdd54abee9e57b68d
fwcd <fwcdmail@gmail.com>
2019-03-08 15:37:34 +0100

    Added more data type declarations and common macros

--- src/CalcManager/win_data_types_cross_platform.h
+++ src/CalcManager/win_data_types_cross_platform.h
@@ -7,6 +7,7 @@ typedef int INT;
 typedef char CHAR;
 typedef long LONG;
 typedef unsigned int UINT_PTR;
+typedef unsigned long ULONG_PTR;
 typedef unsigned int ULONG32;
 typedef unsigned long DWORD;
 typedef unsigned long ULONG;
@@ -14,3 +15,9 @@ typedef unsigned short USHORT;
 typedef wchar_t WCHAR;
 typedef unsigned short WORD;
 typedef UINT_PTR WPARAM;
+typedef ULONG_PTR DWORD_PTR;
+
+#define LOWORD(dw) ((WORD)(((DWORD_PTR)(dw)) & 0xffff))
+#define HIWORD(dw) ((WORD)((((DWORD_PTR)(dw)) >> 16) & 0xffff))
+#define LODWORD(qw) ((DWORD)(qw))
+#define HIDWORD(qw) ((DWORD)(((qw) >> 32) & 0xffffffff))
