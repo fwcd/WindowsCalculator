4f068dae2d216c2f93c12a42dad54f3a0319603d
fwcd <fwcdmail@gmail.com>
2019-03-28 00:04:40 +0100

    Exclude SAL annotation redefinitions from MSVC build

--- src/CalcManager/sal_cross_platform.h
+++ src/CalcManager/sal_cross_platform.h
@@ -2,6 +2,8 @@
 
 // Empty macro definitions for source annotations
 
+#if !defined(_WIN32) || !defined(_MSC_VER)
+
 #define _In_opt_
 #define _Out_opt_
 #define _In_
@@ -9,3 +11,5 @@
 #define _Inout_
 #define __in_opt
 #define _Frees_ptr_opt_
+
+#endif
